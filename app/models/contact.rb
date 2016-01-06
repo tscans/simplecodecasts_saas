de9a679ce892c60d42ae09c4f77435c261c7ddcb
tscanlan@mchs.org <tscanlan@mchs.org>
2016-01-06 03:02:19 +0000

    added validation

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,8 +1,4 @@
 class Contact < ActiveRecord::Base
-    def name
-    end
-    def email
-    end
-    def comments
-    end
+    validates :name, presence: true
+    validates :email, presence: true
 end
