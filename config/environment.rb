544a89169b875a2ac4c9563ba4eaef6b5888bbea
tscanlan@mchs.org <tscanlan@mchs.org>
2016-01-09 01:42:12 +0000

    I mispelled smtp

--- config/environment.rb
+++ config/environment.rb
@@ -6,7 +6,7 @@ Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
     
-    :address => 'smtp_sendgrid.net',
+    :address => 'smtp.sendgrid.net',
     :port => '587',
     :authentication => :plain,
     :user_name => ENV['SENDGRID_USERNAME'],
