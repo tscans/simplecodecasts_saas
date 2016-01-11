80e95c80fb704cbbf1db2d6ff43d92577fb22488
tscanlan@mchs.org <tscanlan@mchs.org>
2016-01-11 02:41:36 +0000

    Added association for plans and users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160110195717) do
+ActiveRecord::Schema.define(version: 20160111023829) do
 
   create_table "contacts", force: true do |t|
     t.string   "name"
