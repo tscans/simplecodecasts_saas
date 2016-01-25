849347316cedad7de69f48eb19e2ebb3f4385c11
tscanlan@mchs.org <tscanlan@mchs.org>
2016-01-25 01:22:06 +0000

    Reset migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160117003126) do
+ActiveRecord::Schema.define(version: 20160125012122) do
 
   create_table "contacts", force: true do |t|
     t.string   "name"
